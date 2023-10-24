6901fa17f095ce167372821a678510a987086efd
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-10-23 20:03:15 -0700

    redo after project)

--- server.js
+++ server.js
@@ -1,77 +1,60 @@
-const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
-// require and set express to app use
-const express = require('express');
-const app = express();
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3001
+const db = require('./db/db.json')
+const { v4: uuidv4 } = require('uuid');
 
-const db = require('./db/db.json');
-//express middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
+//middleware
+app.use(express.static('public'))
+app.use(express.json())
 
+//API Routes
 app.get('/api/notes', (req, res) => {
-    res.json(db.slice(1));
-});
-//get route for index html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-//get route for notes html
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-//wildcard route
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-//create a new note 
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
+    fs.readFile('./db/db.json', (err, data) => {
+        ///error logging
+        if (err) throw err;
+        let dbData = JSON.parse(data);
+        //Returns new database
+        res.json(dbData)
+    });   
+})
 
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
-//post route 
+//Post routes
 app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
-});
-//delete note route
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
+    const newNote = req.body
+    newNote.id = uuidv4()
+    db.push(newNote)
+    fs.writeFileSync('./db/db.json', JSON.stringify(db))
+    res.json(db)
+})
 
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
 
-            break;
-        }
-    }
-}
-//delete route for notes
+//delete route
 app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, db);
-    res.json(true);
-});
+    const noteData = db.filter((note) =>
+        note.id !== req.params.id)
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteData))
+    readFile.json(noteData)
+})
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+//HTML Routes
+//Home
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+//Notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+})
+
+//Wildcard Route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+//App listens with front end on this port
+app.listen(PORT, () =>
+    console.log(`App listening on ${PORT}`))
